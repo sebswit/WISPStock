f6c1e502c654efbf657b10c4aa36eee32b6b2ac5
sebswit <sebastian.switakowski@gmail.com>
2024-06-24 22:33:55 +0100

    Added product schemas with data handling and basic layout

--- src/plugins/service-wispstock/views/betterportal/product-id/index.ts
+++ src/plugins/service-wispstock/views/betterportal/product-id/index.ts
@@ -4,19 +4,49 @@ import {
   BetterPortalUIComponentDataHandler,
   HTTP_DATA_METHOD,
 } from "@bettercorp/betterportal";
-import {z} from "zod";
-import {Plugin} from "../../../plugin";
-import {T_SUPPORTED_THEMES} from "../index";
+import { z } from "zod";
+import { Plugin } from "../../../plugin";
+import { T_SUPPORTED_THEMES } from "../index";
 
-const querySchema = z.object({
+const querySchema = z.object({});
+// schemas
+const productAdditionalInfoSchema = z.object({
+  descriptionShort: z.string().optional(),
+  descriptionLong: z.string().optional(),
+  descriptionLongHtml: z.string().optional(),
+  images: z.array(z.string()),
+  urls: z.record(z.string().url()),
+  // created: z.number().optional(),
+  // published: z.number().optional(),
 });
+
+const distributorSchema = {
+  key: z.string(),
+  name: z.string(),
+  icon: z.string(),
+  url: z.string(),
+};
+const productSchema = {
+  img: z.string(),
+  brand: z.string(),
+  categories: z.array(z.string()),
+  title: z.string(),
+  sku: z.string(),
+  status: z.string(),
+  additionalInfo: productAdditionalInfoSchema,
+};
+
+// main schema
+
 const outputSchema = z.object({
+  ...productSchema,
+  distributors: z.array(z.object(distributorSchema)),
 });
 
 export interface ViewDefinition
-    extends BetterPortalUIComponentBaseTypeDefinition {
-  context?: Plugin,
-  path: "/products/:id/",
+  extends BetterPortalUIComponentBaseTypeDefinition {
+  context?: Plugin;
+  path: "/products/:id/";
   query: typeof querySchema;
   inboundData: null;
   outputData: typeof outputSchema;
@@ -28,24 +58,57 @@ export const ViewSchema: ViewDefinition = {
   query: querySchema,
   inboundData: null,
   outputData: outputSchema,
-  methods: [
-    "GET",
-  ],
+  methods: ["GET"],
 };
 
-import {Component as Materio1} from "./materio1";
+import { Component as Materio1 } from "./materio1";
+import { ProductListItem } from "src";
 
-export const ViewHandlers: Record<T_SUPPORTED_THEMES, BetterPortalUIView<ViewDefinition> | null> = {
+export const ViewHandlers: Record<
+  T_SUPPORTED_THEMES,
+  BetterPortalUIView<ViewDefinition> | null
+> = {
   materio1: Materio1,
 };
 
-export const DataHandler: BetterPortalUIComponentDataHandler<ViewDefinition> = async (props) => {
+export const DataHandler: BetterPortalUIComponentDataHandler<
+  ViewDefinition
+> = async (props) => {
+  const session = props.context.RavenDB.openSession();
+
+  const product = await session.load<ProductListItem>(props.params.id);
+
+  if (product == null)
+    return {
+      status: 404,
+      content: null,
+    };
+
   return {
     status: 200,
-    content: {} as any,
+    content: {
+      sku: product.sku,
+      title: product.title,
+      brand: product.brand,
+      img: product.img,
+      categories: product.categories,
+      additionalInfo: {
+        descriptionLong: product.additionalInfo.descriptionLong,
+        descriptionShort: product.additionalInfo.descriptionShort,
+        delscriptionLongHtml: product.additionalInfo.descriptionLongHtml,
+        images: product.additionalInfo.images,
+        urls: product.additionalInfo.urls,
+      },
+      status: product.status,
+      distributors: [],
+    } as z.infer<typeof outputSchema>,
   };
 };
-export const DataHandlers: Record<HTTP_DATA_METHOD, BetterPortalUIComponentDataHandler<ViewDefinition> | undefined> = {
+
+export const DataHandlers: Record<
+  HTTP_DATA_METHOD,
+  BetterPortalUIComponentDataHandler<ViewDefinition> | undefined
+> = {
   POST: DataHandler,
   GET: DataHandler,
   PATCH: undefined,
--- src/plugins/service-wispstock/views/betterportal/product-id/materio1.tsx
+++ src/plugins/service-wispstock/views/betterportal/product-id/materio1.tsx
@@ -1,17 +1,119 @@
 /** @jsxImportSource jsx-htmx */
 
-import {BetterPortalUIView} from "@bettercorp/betterportal";
-import {
-  ViewDefinition,
-} from "./index";
+import { BetterPortalUIView } from "@bettercorp/betterportal";
+import { ViewDefinition } from "./index";
 
 export const Component: BetterPortalUIView<ViewDefinition> = (props) => {
+  const product = props.outboundData;
+
   return {
     status: 200,
     content: (
-        <div class="row">
-          PRODUCT {props.params.id}
+      <div class="row" style="height:75vh">
+        <div
+          class="d-flex shadow p-3 rounded bg-gradient"
+          style="width: 100vw;"
+        >
+          <div class="p-3 rounded-3 w-50">
+            <img
+              src={product.img}
+              class=" rounded-3 w-100 "
+              alt={product.title}
+            />
+            <h1 class="card-title text-center user-select-none">
+              {product.title}
+            </h1>
+          </div>
+
+          <div class="card-body d-flex flex-column w-50 ">
+            <p class="card-text user-select-none">
+              {product.additionalInfo.descriptionShort}
+            </p>
+
+            <ul class="list-group list-group-flush mb-3">
+              <li class="list-group-item pe-none user-select-none">
+                Brand: {product.brand}
+              </li>
+              <li class="list-group-item pe-none user-select-none">
+                SKU: {product.sku}
+              </li>
+              <li
+                class={`list-group-item pe-none user-select-none ${
+                  !product.categories || product.categories.length === 0
+                    ? "d-none"
+                    : ""
+                }`}
+              >
+                Category: {product.categories || "No categories"}
+              </li>
+              <li
+                class={`list-group-item pe-none user-select-none ${
+                  !product.status || product.status.length === 0 ? "d-none" : ""
+                }`}
+              >
+                Status: {product.status || "No status"}
+              </li>
+              <li class="list-group-item pe-none user-select-none">
+                Total stock: {}
+              </li>
+              <li class="list-group-item pe-none user-select-none">
+                Distributors: {}
+              </li>
+            </ul>
+
+            {(product.additionalInfo.descriptionLong ||
+              product.additionalInfo.descriptionLongHtml) && (
+              <>
+                <button
+                  class="btn btn-primary mb-3"
+                  type="button"
+                  style="min-height: 50px"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#collapseWidthExample"
+                  aria-expanded="false"
+                  aria-controls="collapseWidthExample"
+                >
+                  Product additional info
+                </button>
+                <div style="min-height: 50px;">
+                  <div
+                    class="collapse collapse-horizontal"
+                    id="collapseWidthExample"
+                  >
+                    <div class="card card-body" style="width: 40vw">
+                      {product.additionalInfo.descriptionLongHtml ||
+                        product.additionalInfo.descriptionLong}
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
+
+            <div class="mt-auto d-flex justify-content-center align-self-center">
+              <div class="d-flex gap-2 ">
+                <div class="flex-row">
+                  <div
+                    data-mdb-input-init
+                    class="form-outline "
+                    style="width: 22rem;"
+                  >
+                    <input
+                      type="number"
+                      id="typeNumber"
+                      class="form-control"
+                      placeholder="Input quantity"
+                      onchange="this.value = this.value < 0 ? 0 : this.value;"
+                    />
+                  </div>
+                </div>
+                <button type="button" class="btn btn-primary btn-m">
+                  Add to basket
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
     ),
   };
 };
